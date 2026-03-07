4ff0385f89b5b1c7c109a065c9a2b3bb223ac632
Zulkifal05 <zulkifalali299@gmail.com>
2026-03-07 14:54:32 +0500

    Password filed removed from querry

--- NextJSApp/src/utils/VerifyJWT.ts
+++ NextJSApp/src/utils/VerifyJWT.ts
@@ -8,7 +8,7 @@ export const verifyJWT = async (token: string) => {
         const decoded = jwt.verify(token, process.env.NEXT_JWT_SECRET as string) as DefaultPayload
 
         if(decoded) {
-            const user = await UserModel.findById(decoded?.id)
+            const user = await UserModel.findById(decoded?.id).select("-password")
             if(!user) {
                 return null
             }
