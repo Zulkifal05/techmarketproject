552d867ebf177d65621bc194400ef8022f69670b
Zulkifal05 <zulkifalali299@gmail.com>
2026-03-08 14:07:27 +0500

    Jwt verification decoded token payload peremeter updated

--- NextJSApp/src/utils/VerifyJWT.ts
+++ NextJSApp/src/utils/VerifyJWT.ts
@@ -8,12 +8,12 @@ export const verifyJWT = async (token: string) => {
         const decoded = jwt.verify(token, process.env.NEXT_JWT_SECRET as string) as DefaultPayload
 
         if(decoded) {
-            const user = await UserModel.findById(decoded?.id).select("-password")
+            const user = await UserModel.findById(decoded?.userId).select("-password")
             if(!user) {
                 return null
             }
 
-            return decoded.id
+            return user
         }
     } catch (error) {
         console.error("JWT verification failed:", error)
