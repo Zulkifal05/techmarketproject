14fa2482649d0e00704312f02b0a966916093e34
Zulkifal05 <zulkifalali299@gmail.com>
2026-03-09 14:32:29 +0500

    Delete job route created

---
+++ NextJSApp/src/app/api/Job/Delete-Job/route.ts
@@ -0,0 +1,60 @@
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
+import { verifyJWT } from "@/utils/VerifyJWT"
+import JobModel from "@/models/JobModel"
+import { DeleteJobSchema } from "@/schemas/JobSchema"
+
+export async function DELETE(req: Request) {
+    try {
+        const cookieStore = await cookies()
+        const token = cookieStore.get("token")?.value
+
+        if (!token) {
+            return NextResponse.json({ message: "Unauthorized: No token provided", success: false }, { status: 401 })
+        }
+
+        const user = await verifyJWT(token)
+
+        if (!user) {
+            return NextResponse.json({ message: "Unauthorized: Invalid token", success: false }, { status: 401 })
+        }
+
+        if(user.role !== "BUYER") { 
+            return NextResponse.json({ message: "Only BUYERS are authorized", success: false }, { status: 403 })
+        }
+
+        const { jobId } = await req.json()
+
+        if (!jobId) {
+            return NextResponse.json({ message: "Job ID is required", success: false }, { status: 400 })
+        }
+
+        const validatedData = DeleteJobSchema.safeParse({ jobId })
+
+        if (!validatedData.success) {
+            return NextResponse.json({ message: `Validation error`,errors: validatedData.error.flatten().fieldErrors, success: false }, { status: 400 })
+        }
+
+        const job = await JobModel.findById(jobId)
+
+        if (!job) {
+            return NextResponse.json({ message: "Job not found", success: false }, { status: 404 })
+        }
+
+        if (job.uploadedBy.toString() !== user.id) {
+            return NextResponse.json({ message: "You are not authorized to delete this job", success: false }, { status: 403 })
+        }
+
+        await JobModel.findByIdAndDelete(jobId)
+
+        return NextResponse.json({ message: "Job deleted successfully", success: true }, { status: 200 })
+    } catch (error) {
+        console.error("Error deleting job:", error)
+
+        if(error instanceof SyntaxError) {
+            return NextResponse.json({ message: "Invalid JSON in request body", success: false }, { status: 500 })
+        }
+
+        return NextResponse.json({ message: "Failed to delete job", success: false }, { status: 500 })
+    }
+}
