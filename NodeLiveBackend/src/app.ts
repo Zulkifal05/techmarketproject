3c37436924c14cdb7cfd27c52a96323fab08af26
Zulkifal05 <zulkifalali299@gmail.com>
2026-03-17 15:59:08 +0500

    Created express app

--- NodeLiveBackend/src/app.ts
+++ NodeLiveBackend/src/app.ts
@@ -1,3 +1,21 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-const app = express()
+export const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({ 
+    origin : process.env.CLIENT_URL,
+    credentials : true
+}))
+
+app.get("/", async (req,res) => {
+    res.send(`Hello!`)
+})
+
+import messagesRouter from "./routes/messages.routes.js"
+
+app.use("/api/messages",messagesRouter)
