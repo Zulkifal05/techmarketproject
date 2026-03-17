335ce78e60c7b57781da9bb4e7f93c19d22903ea
Zulkifal05 <zulkifalali299@gmail.com>
2026-03-17 15:54:14 +0500

    Declare optional global user in request type in express

---
+++ NodeLiveBackend/src/types/types.d.ts
@@ -0,0 +1,9 @@
+import { User } from "../models/user.model.ts"
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User; // optional because middleware might not set it
+    }
+  }
+}
