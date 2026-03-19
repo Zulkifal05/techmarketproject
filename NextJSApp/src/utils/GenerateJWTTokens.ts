6899f1e6a82ed7215601a2d79663afcb5c882028
Zulkifal05 <zulkifalali299@gmail.com>
2026-03-19 11:59:58 +0500

    Utility function for generating access and refresh tokens

---
+++ NextJSApp/src/utils/GenerateJWTTokens.ts
@@ -0,0 +1,31 @@
+import jwt, { SignOptions } from "jsonwebtoken"
+import UserModel from "@/models/UserModel"
+
+async function GenerateAccessAndRefreshToken (userId:string) {
+    try {
+        const user = await UserModel.findById(userId)
+
+        if(!user) {
+            throw new Error("User with ID not found")
+        }
+
+        const payload = { userId };
+        const secret = process.env.NEXT_JWT_SECRET!;
+
+        const accessTokenoptions: SignOptions = { expiresIn : "15m" };
+        const refreshTokenoptions: SignOptions = { expiresIn : "10d" };
+        
+        const accessToken = jwt.sign(payload, secret, accessTokenoptions);
+        const refreshToken = jwt.sign(payload, secret, refreshTokenoptions);
+
+        user.refreshToken = refreshToken
+        await user.save()
+
+        return { accessToken , refreshToken }
+    } catch (e) {
+        console.error(e)
+        throw new Error("JWT Token Creation Error")
+    }
+}
+
+export default GenerateAccessAndRefreshToken
