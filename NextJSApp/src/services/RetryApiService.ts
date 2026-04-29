8948fe386e9f7e9004b5a4533e8e96d06e30cb84
Zulkifal05 <zulkifalali299@gmail.com>
2026-04-29 18:41:41 +0500

    Create seperate function for retry auth failed api

---
+++ NextJSApp/src/services/RetryApiService.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosRequestConfig } from "axios"
+import AuthService from "./AuthService"
+
+type methodType = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+
+// This function can be used to retry API calls that failed due to authentication issues, such as an expired token. It can be called after refreshing the token to attempt the original API call again.
+async function ReftryAuthFailedApiCall(method: methodType, url: string, data?: unknown) {
+    try {
+        const newAccessToken : string = await AuthService.RefreshToken();
+
+        if(newAccessToken) {
+            // If the token refresh was successful, retry the original API call
+
+            const config: AxiosRequestConfig = {
+                method,
+                url,
+                headers: {
+                Authorization: `Bearer ${newAccessToken}`,
+                },
+            };
+
+            if (data && method !== "GET") {
+                config.data = data;
+            }
+
+            const retryResponse = await axios.request(config);
+
+            if(retryResponse) {
+                return retryResponse; // Return the response data from the retried API call
+            }
+        } else {
+            throw new Error("Token refresh failed");
+        }
+    } catch (error) {
+        console.error("Error retrying API call:", error);
+        return null;
+    }
+}
+
+export default ReftryAuthFailedApiCall
