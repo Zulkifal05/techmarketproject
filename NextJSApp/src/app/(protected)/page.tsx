3a3350d147e08682abd6780991076b061992b0f7
Zulkifal05 <zulkifalali299@gmail.com>
2026-05-14 20:34:59 +0500

    Create main page and layout for developer but little work remaining for hirer's view

--- NextJSApp/src/app/(protected)/page.tsx
+++ NextJSApp/src/app/(protected)/page.tsx
@@ -1,28 +1,37 @@
-'use client'
+import CategoriesBanner from "@/components/mainpage/CategoriesBanner"
+import Feed from "@/components/mainpage/Feed"
+import HirerFeatures from "@/components/mainpage/HirerFeatures"
+import { getUser } from "@/utils/GetUser"
+import { redirect } from "next/navigation"
 
-import React from 'react'
-import toast from 'react-hot-toast'
-import authService from '../../services/AuthService'
+export default async function Page() {
+  const user = await getUser()
 
-export default function page() {
-  const handleMockSignup = async () => {
-    const mockUsername = 'mockuser'
-    const mockEmail = 'mockuser@example.com'
-    const mockPassword = 'Password123!'
-    const mockRole = 'SELLE'
-
-    const user = await authService.SignUp(mockUsername, mockEmail, mockPassword, mockRole)
-
-    if (user) {
-      console.log('Mock signup success:', user)
-      toast.success('Mock signup completed successfully')
-    } else {
-      console.log('Mock signup failed')
-      toast.error('Mock signup failed')
-    }
+  if (!user) {
+    redirect("/login")
   }
 
   return (
-    <button onClick={handleMockSignup}>Run Mock Signup</button>
+    <>
+      <div className="bg-linear-to-r from-blue-600 to-purple-600 text-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+          <div className="max-w-3xl">
+            <h1 className="text-4xl sm:text-5xl font-bold mb-4">
+              Welcome to TechMaket
+            </h1>
+            <p className="text-lg sm:text-xl text-blue-100 mb-8">
+              Your one-stop platform for connecting developers and clients to build, hire, and grow in tech.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      { user?.role === "SELLER" ? 
+        <>
+          <CategoriesBanner />
+          <Feed />
+        </> : 
+        <HirerFeatures /> }
+    </>
   )
 }
