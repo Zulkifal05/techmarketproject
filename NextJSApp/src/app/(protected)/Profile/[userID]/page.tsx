cd4eff6552b30ef8a3fa951e9b1a1529757d4ef2
Zulkifal05 <zulkifalali299@gmail.com>
2026-05-16 15:31:03 +0500

    Profile page completed for hirer's view but work remaining for developer's view and not-found page specific to profile also added

---
+++ NextJSApp/src/app/(protected)/Profile/[userID]/page.tsx
@@ -0,0 +1,64 @@
+import { Mail } from "lucide-react"
+import Image from "next/image"
+import { verifyJWT } from "@/utils/VerifyJWT"
+import { cookies } from "next/headers";
+import { notFound, redirect } from "next/navigation"
+import GetSpecificUser from "@/services/server/GetSpecificUser"
+import JobsPosted from "@/components/profile/JobsPosted"
+
+export default async function ProfilePage({ params }: { params: Promise<{ userID: string }> }) {
+
+    const { userID } = await params;
+    const cookieStore = await cookies()
+    const token = cookieStore.get("accessToken")?.value as string;
+
+    const loggedInUser = await verifyJWT(token)
+
+    if(!loggedInUser) {
+      redirect("/Login");
+    }
+
+    const userProfile = await GetSpecificUser(userID);
+
+    if(!userProfile) {
+      notFound();
+    }
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+
+      {/* Profile Header */}
+      <div className="bg-linear-to-r from-blue-600 to-purple-600">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+          <div className="flex flex-col md:flex-row items-start md:items-center gap-6">
+            {/* Profile Picture */}
+            <div className="w-32 h-32 bg-white rounded-2xl flex items-center justify-center shadow-xl">
+              <Image
+                src="/Default-Avatar.png"
+                alt="Profile Picture"
+                className="w-28 h-28 rounded-xl object-cover"
+                width={112}
+                height={112}
+              />
+            </div>
+
+            {/* Profile Info */}
+            <div className="flex-1 text-white">
+              <h1 className="text-3xl font-bold mb-2">{userProfile?.name}</h1>
+              <p className="text-xl text-blue-100 mb-4">{userProfile?.role === "BUYER" ? "Hirer" : "Developer"}</p>
+
+              <div className="flex flex-wrap gap-4 text-sm text-blue-100">
+                <div className="flex items-center gap-2">
+                  <Mail className="w-4 h-4" />
+                  {userProfile?.email}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <JobsPosted userId={String(userProfile?._id)} userAllowedToUpdate={String(loggedInUser?._id) === String(userProfile?._id)}/>
+      </div>
+      )
+}
