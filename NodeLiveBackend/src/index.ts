01064b9abf0122f4ea53c5b5b79c8717ee1a69e0
Zulkifal05 <zulkifalali299@gmail.com>
2026-05-23 12:15:10 +0500

    Init socket.io

--- NodeLiveBackend/src/index.ts
+++ NodeLiveBackend/src/index.ts
@@ -5,6 +5,7 @@ import { connectDB } from "./utils/db.js"
 import { Server } from "socket.io"
 import http from 'http'
 import { app } from "./app.js"
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const PORT = process.env.PORT || 5000
 const server = http.createServer(app);
@@ -25,23 +26,60 @@ connectDB().then(() => {
     process.exit(1);
 })
 
-// Object to store online users
-const onlineUsers: Record<string, string> = {};
+// ------------------- Socket.IO Authentication and Online Users Management -------------------
 
-export function getReceiverSocketId(userId: string): string | undefined {
-    return onlineUsers[userId];
+// Object to store online users
+const onlineUsers: Record<string, Set<string>> = {};
+
+// Socket.io middleware to authenticate users based on their access token
+interface MyJwtPayload extends JwtPayload {
+  userId: string;
+}
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+
+  if (!token) return next(new Error("No token"));
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as MyJwtPayload;
+
+    if(!decoded.userId) {
+        return next(new Error("Token Payload Issue"));
+    }
+    
+    socket.user = decoded; // attach verified user info to socket object
+    next();
+  } catch (err) {
+    next(new Error("Unauthorized"));
+  }
+});
+
+export function getReceiverSocketId(userId: string): string[] | undefined {
+    return onlineUsers[userId] ? Array.from(onlineUsers[userId]) : [];
 }
 
 // Socket.IO connection handling
 io.on('connection', (socket) => {
-    // Listen for user login and store their socket ID
-    const { userId } = socket.handshake.auth;
-    onlineUsers[userId] = socket.id;
+    const userId = socket.user.userId;
+
+    // initialize set if user not exists
+    if (!onlineUsers[userId]) {
+        onlineUsers[userId] = new Set();
+    }
+
+    // add this socket to user's set
+    onlineUsers[userId].add(socket.id);
 
     socket.on('disconnect', () => {
-        // Remove the user from the online users list
-        delete onlineUsers[userId];
+        // remove this socket from user's set
+        onlineUsers[userId]?.delete(socket.id);
+
+        // cleanup if no sockets left
+        if (onlineUsers[userId]?.size === 0) {
+            delete onlineUsers[userId];
+        }
     });
-})
+});
 
 export { io }
