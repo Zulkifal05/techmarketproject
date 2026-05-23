65600d26fddaaa3e1ff2e30a158143ab01d007e0
Zulkifal05 <zulkifalali299@gmail.com>
2026-05-23 12:33:31 +0500

    Add get user chats route

--- NodeLiveBackend/src/routes/messages.routes.ts
+++ NodeLiveBackend/src/routes/messages.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { GetMessages, SendMessage } from "../controllers/messages.controller.js"
+import { GetMessages, GetUserChats, SendMessage } from "../controllers/messages.controller.js"
 import { AuthCheck } from "../middlewares/auth.middleware.js"
 
 const messagesRouter = Router()
 
 messagesRouter.post("/Send-Message",AuthCheck,SendMessage)
 messagesRouter.get("/Get-Messages",AuthCheck,GetMessages)
+messagesRouter.get("/Get-User-Chats",AuthCheck,GetUserChats)
 
 export default messagesRouter
