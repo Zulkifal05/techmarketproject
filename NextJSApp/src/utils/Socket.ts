ddd0947037c86c1a953e6316fde1f23c98b16497
Zulkifal05 <zulkifalali299@gmail.com>
2026-07-08 15:18:19 +0500

    Complete socket utility

--- NextJSApp/src/utils/Socket.ts
+++ NextJSApp/src/utils/Socket.ts
@@ -1,12 +1,34 @@
 import { io , Socket } from "socket.io-client"
+import axios from "axios"
+
+interface TokenResponse {
+  accessToken: string;
+  status?: number;
+  success?: boolean;
+}
 
 let socket: Socket | null = null;
 
-function getSocket() {
+async function getSocket() {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
-      autoConnect: false,
-    });
+    try {
+      const tokenResponse = await axios.get<TokenResponse>("/api/Auth/Get-Token", { withCredentials: true });
+
+      socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
+        autoConnect: false,
+        auth: {
+          token: tokenResponse.data.accessToken
+        }
+      });
+
+      return socket;
+    } catch (error) {
+      if(axios.isAxiosError(error) && (error.response?.status === 400 || error.response?.status === 500)) {
+        return null; // Return null if token fetch fails
+      }
+
+      return null; // Return null for any other errors
+    }
   }
 
   return socket;
